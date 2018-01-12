75c67d32c5f644d7f3d0e358bc25cda6ad9e05ad
Adam Calhoun <a.t.calhoun@gmail.com>
2018-01-12 09:52:54 -0800

    test getters -- pass

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
+  List<String> members = new ArrayList<>();
+  String description;
+  String name;
 
-  public Team(){
+  public Team(String teamName, List<String> people, String desc){
+    name = teamName;
+    description = desc;
+    members = people;
+  }
 
+  public String getName() {
+    return name;
+  }
+  public List<String> getMembers() {
+    return members;
+  }
+  public String getDescription() {
+    return description;
   }
 }
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -1,11 +1,40 @@
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class TeamTest {
-  Team testTeam = new Team();
+  String name = "Team";
+  String desc = "The Best Team";
+  List<String> members = new ArrayList<String>(){{
+    add("Bobson Dugnutt");
+    add("Todd Bonzalez");
+    add("Mike Truk");
+    add("Willie Dustice");
+  }};
+
+
+  Team testTeam = new Team(name, members, desc);
 
   @Test
   public void Team_instantiates_bool(){
     assertTrue(testTeam != null);
+  }
+
+  @Test
+  public void getName_getsName_string(){
+    assertEquals("Team", testTeam.getName());
+  }
+
+  @Test
+  public void getDescription_getsDescription_string(){
+    assertEquals("The Best Team", testTeam.getDescription());
+  }
+
+  @Test
+  public void getMembers_getListOfMembers_List(){
+    assert(testTeam.getMembers().size() > 2);
   }
 }
