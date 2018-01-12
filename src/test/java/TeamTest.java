3984e4748b5deceec0f84e95bf80f8835ad07919
Adam Calhoun <a.t.calhoun@gmail.com>
2018-01-12 09:09:10 -0800

    pass instantiation test

--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -1,2 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TeamTest {
+  Team testTeam = new Team();
+
+  @Test
+  public void Team_instantiates_bool(){
+    assertTrue(testTeam != null);
+  }
 }
