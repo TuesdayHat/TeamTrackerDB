de63eda75d311edddb197b10e90d256a672b890f
Guest User <Guest@Epicodus-6C.local>
2018-01-19 09:22:48 -0800

    initial commit -- independent project Jan 19 2018

---
+++ src/main/java/Member.java
@@ -0,0 +1,18 @@
+
+
+
+public class Member {
+    private String name;
+    private int id;
+    private int teamId;
+
+    public Member(String name){
+        this.name = name;
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
