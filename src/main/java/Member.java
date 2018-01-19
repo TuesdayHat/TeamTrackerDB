a347265041b3ad3a36114c0fa6a79f51c04230a7
tuesdayhat <a.t.calhoun@gmail.com>
2018-01-19 09:41:29 -0800

    create Member class

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -11,8 +11,46 @@ public class Member {
 
     }
 
+    public void setName(String newName){
+        this.name = newName;
+    }
+    public void setId(int newId){
+        this.id = newId;
+    }
+    public void setTeamId(int newTeamId){
+        this.teamId = newTeamId;
+    }
+
+    //GETTERS
     public String getName(){
         return name;
+    }
+    public int getId(){
+        return id;
+    }
+    public int getTeamId(){
+        return teamId;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return false;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Member member = (Member) o;
+
+        if (id != member.id) return false;
+        if (teamId != member.teamId) return false;
+        return name.equals(member.name);
+    }
+
+    @Override
+    public int hashCode(){
+        int result = name.hashCode();
+        result = 31*result+id;
+        result = 31*result+teamId;
+
+        return result;
     }
 
 }
